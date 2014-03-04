e7c68fc27cb72f8d79a5c0521a0c3b9dcf592bcb
erickpires <rckkas@gmail.com>
2014-03-04 02:04:53 -0300

    first commit

---
+++ Assets/BehaviourScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class BehaviourScript : MonoBehaviour {
+
+	public float forwardSpeed;
+	public float gravity;
+	public float jumpHeight;
+	public float sideSpeed;
+	
+	Vector3 movementVelocity = Vector3.zero;
+	bool jumping = false;
+	bool onGround;
+	bool crouching;
+	// Use this for initialization
+	void Start () {
+
+		animation.Play("run");
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Variable Initiation
+		CharacterController controller = (CharacterController)GetComponent<CharacterController>();
+		GamePadState state = GamePad.GetState(0);
+		onGround = transform.position.y > 0.1 ? false : true;//controller.isGrounded;
+		
+		movementVelocity = (transform.forward * Time.deltaTime * forwardSpeed);
+
+		if(Input.GetKey(KeyCode.LeftArrow) || state.ThumbSticks.Left.X == -1){
+		  	movementVelocity -= Vector3.right * sideSpeed;
+		}
+		if(Input.GetKey(KeyCode.RightArrow) || state.ThumbSticks.Left.X == 1){
+			movementVelocity += Vector3.right * sideSpeed;
+		}
+
+		if (Input.GetKey (KeyCode.LeftControl) || state.Buttons.B == ButtonState.Pressed)
+			crouching = true;
+		else
+			crouching = false;
+			
+		if (!jumping && (Input.GetKey (KeyCode.Space) || state.Buttons.A == ButtonState.Pressed)) {
+			jumping = true;
+				movementVelocity.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+		}
+
+
+		if (jumping && onGround && !Input.GetKey(KeyCode.Space) && state.Buttons.A == ButtonState.Released){
+			jumping = false;
+		}
+
+		Animate();
+		
+		ApplyGravity();
+		
+		controller.Move(movementVelocity * Time.deltaTime);
+	}
+	
+	void ApplyGravity(){
+		if(!onGround)
+			movementVelocity.y -= gravity * Time.deltaTime;
+			
+	}
+	
+	void Animate(){
+		if (!onGround)
+			animation.Play ("jump");
+		else if(crouching)
+			animation.Play("crouchRun");
+		else
+			animation.Play ("run");
+	}
+}
---
+++ Assets/CameraBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class CameraBehaviourScript : MonoBehaviour {
+	public GameObject player;
+	public float distanceFromPlayer;
+	public float cameraHeight;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos = Vector3.zero;
+		pos.z = player.transform.position.z - distanceFromPlayer;
+		pos.y = cameraHeight;
+		transform.position = pos;
+		
+		transform.forward = player.transform.position - pos;
+	}
+}
