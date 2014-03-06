6120f496386d508e47b22235836c34c33659e03f
erickpires <rckkas@gmail.com>
2014-03-06 19:12:21 -0300

    Gold is collected, onGround logic modified, Player Collider is adjusted when crouching, Zombies can move and kill the Player

--- Assets/BehaviourScript.cs
+++ Assets/BehaviourScript.cs
@@ -7,12 +7,19 @@ using XInputDotNetPure;
 */
 
 public class BehaviourScript : MonoBehaviour {
-
+	
 	public float forwardSpeed;
 	public float gravity;
 	public float jumpHeight;
 	public float sideSpeed;
-	public GUIText counter;
+	public float standingHeight;
+	public float crouchingHeight;
+	
+	public Vector3 standingCenter;
+	public Vector3 crouchingCenter;
+	
+	public GUIText score;
+	public GUIText goldText;
 	
 	Vector3 movementVelocity = Vector3.zero;
 	bool hasJumped = false;
@@ -21,16 +28,18 @@ public class BehaviourScript : MonoBehaviour {
 	bool isAlive;
 	
 	float distance;
-	GameObject textObject = null;
+	int gold;
+
 	// Use this for initialization
 	void Start () {
-
+		
 		animation.Play("run");
 		
 		distance = 0;
 		isAlive = true;
 		
-		
+		gold = 0;
+		goldText.text = "Ouro: " + gold;
 	}
 	
 	// Update is called once per frame
@@ -38,61 +47,54 @@ public class BehaviourScript : MonoBehaviour {
 		//Variable Initiation
 		CharacterController controller = (CharacterController)GetComponent<CharacterController>();
 		GamePadState state = GamePad.GetState(0);
-		onGround = transform.position.y > 0.1 ? false : true;//controller.isGrounded;
 		
 		if(isAlive){
-		movementVelocity = (transform.forward * Time.deltaTime * forwardSpeed);
-
-		if(onGround && (Input.GetKey(KeyCode.LeftArrow) || state.ThumbSticks.Left.X == -1)){
-		  	movementVelocity -= Vector3.right * sideSpeed;
-		}
-		if(onGround && (Input.GetKey(KeyCode.RightArrow) || state.ThumbSticks.Left.X == 1)){
-			movementVelocity += Vector3.right * sideSpeed;
-		}
-
-		if (onGround && (Input.GetKey (KeyCode.LeftControl) || state.Buttons.B == ButtonState.Pressed))
-			crouching = true;
-		else
-			crouching = false;
+			movementVelocity = (transform.forward * Time.deltaTime * forwardSpeed);
 			
-		if ((Input.GetKey (KeyCode.Space) || state.Buttons.A == ButtonState.Pressed) && !hasJumped) {
-			Debug.Log("jumped");
-			hasJumped = true;
-			movementVelocity.y = Mathf.Sqrt(2 * jumpHeight * gravity);
-		}
-
-		if(crouching)
-			controller.height = 1;
-		else
-			controller.height = 2;
-
-
-		if (hasJumped && onGround && !Input.GetKey(KeyCode.Space) && state.Buttons.A == ButtonState.Released){
-			hasJumped = false;
-		}
-
-		Animate();
-		
-		ApplyGravity();
-		
-		controller.Move(movementVelocity * Time.deltaTime);
-		
-		distance += movementVelocity.z * Time.deltaTime;
-		
-		counter.text = string.Format( "Distancia percorrida: {0:0.0} m", distance);
+			if(onGround && (Input.GetKey(KeyCode.LeftArrow) || state.ThumbSticks.Left.X == -1)){
+				movementVelocity -= Vector3.right * sideSpeed;
+			}
+			if(onGround && (Input.GetKey(KeyCode.RightArrow) || state.ThumbSticks.Left.X == 1)){
+				movementVelocity += Vector3.right * sideSpeed;
+			}
+			
+			if (onGround && (Input.GetKey (KeyCode.LeftControl) || state.Buttons.B == ButtonState.Pressed))
+				crouching = true;
+			else
+				crouching = false;
+			
+			if ((Input.GetKey (KeyCode.Space) || state.Buttons.A == ButtonState.Pressed) && !hasJumped) {
+				Debug.Log("jumped");
+				hasJumped = true;
+				onGround = false;
+				movementVelocity.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+			}
+			
+			if(crouching){
+				controller.height = crouchingHeight;
+				controller.center = crouchingCenter;
+			}
+			else{
+				controller.height = standingHeight;
+				controller.center = standingCenter;
+			}
+			
+			
+			if (hasJumped && onGround && !Input.GetKey(KeyCode.Space) && state.Buttons.A == ButtonState.Released){
+				hasJumped = false;
+			}
+			
+			Animate();
+			
+			ApplyGravity();
+			
+			controller.Move(movementVelocity * Time.deltaTime);
+			
+			distance += movementVelocity.z * Time.deltaTime;
+			
+			score.text = string.Format( "Distancia percorrida: {0:0.0} m", distance);
 		}
 		else{
-			if(textObject == null){
-			textObject = new GameObject("Game Over");
-			
-			textObject.transform.position = new Vector3(0.5f, 0.5f, 0);
-			textObject.AddComponent("GUIText");
-			textObject.guiText.text = "Game\n Over";
-			textObject.guiText.fontSize = 70;
-			textObject.guiText.anchor = TextAnchor.MiddleCenter;
-			textObject.guiText.color = Color.red;
-			}
-			//gameObject.SetActive(false);
 			animation.Play("idle");
 			
 			if(Input.GetKey (KeyCode.Return) || state.Buttons.Y == ButtonState.Pressed){
@@ -105,7 +107,7 @@ public class BehaviourScript : MonoBehaviour {
 	void ApplyGravity(){
 		if(!onGround)
 			movementVelocity.y -= gravity * Time.deltaTime;
-			
+		
 	}
 	
 	void Animate(){
@@ -117,8 +119,74 @@ public class BehaviourScript : MonoBehaviour {
 			animation.Play ("run");
 	}
 	
-	void OnTriggerEnter( Collider other){
-		//Debug.Log("Collidiu com " + other);
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		Debug.Log("collided with " + hit.gameObject);
+		if(hit.gameObject.name == "Barrier" || hit.collider.name == "Zombie")
+			Die();
+			
+		if(hit.gameObject.name == "Ground")
+			onGround = true;
+			
+		if(hit.gameObject.name == "Gold Ignot"){
+			gold++;
+			Destroy(hit.gameObject);
+			goldText.text = "Ouro: " + gold;
+		}
+	}
+	
+	void Die (){
+		float record = 0;
+		bool newRecord = false;
+		
+		if(System.IO.File.Exists("record.dat")){
+			Debug.Log("record exists");
+			
+			System.IO.StreamReader streamReader = new System.IO.StreamReader("record.dat");
+			
+			record = float.Parse(streamReader.ReadLine());
+			
+			streamReader.Close();
+		}
+		
+		if(distance > record){
+			Debug.Log("new Record");
+			record = distance;
+			newRecord = true;
+			System.IO.StreamWriter streamWriter = new System.IO.StreamWriter("record.dat");
+			
+			streamWriter.WriteLine(distance.ToString());
+			
+			streamWriter.Close();
+		}
+		
+		ShowGameOverText(record, newRecord);
+		
 		isAlive = false;
+	}
+	
+	void ShowGameOverText(float record, bool isNewRecord){
+		GameObject textObject = new GameObject("Game Over");
+		
+		textObject.transform.position = new Vector3(0.5f, 0.5f, 0);
+		textObject.AddComponent("GUIText");
+		textObject.guiText.fontSize = 70;
+		textObject.guiText.anchor = TextAnchor.MiddleCenter;
+		textObject.guiText.color = Color.red;
+		textObject.guiText.text = "Game\n Over";
+		
+		GameObject textObject2 = new GameObject("Score");
+		
+		string text = string.Format("Recorde atual: {0:0.0} m", record);
+		
+		if(isNewRecord)
+			text = "Novo Recorde\n" + text;
+		
+		textObject2.transform.position = new Vector3(0.5f, 0, 0);
+		textObject2.AddComponent("GUIText");
+		textObject2.guiText.fontSize = 50;
+		textObject2.guiText.anchor = TextAnchor.LowerCenter;
+		textObject2.guiText.alignment = TextAlignment.Center;
+		textObject2.guiText.color = Color.grey;
+		textObject2.guiText.text = text;
 	}
 }
---
+++ Assets/ZombieBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieBehaviourScript : MonoBehaviour {
+
+	public float idleDistance;
+	public float speed;
+	
+	GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		rigidbody.freezeRotation = true;
+		
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 playerDirection = player.transform.position - this.transform.position;
+		playerDirection.y = 0;
+		
+		
+		if(playerDirection.magnitude < idleDistance){
+			rigidbody.velocity = playerDirection.normalized * speed;
+			transform.forward = playerDirection.normalized;
+		}
+		else
+			rigidbody.velocity = Vector3.zero;
+			
+		stopGoingUp();
+	}
+	
+	void stopGoingUp(){
+		Vector3 pos = transform.position;
+		pos.y = 0;
+		transform.position = pos;
+	}
+}
